96fc4cb95e66f809ebfa44aaf12ec779084dbefe
AsadullahQureshi <abduljabbarasadullah@gmail.com>
2024-03-29 04:58:56 +0500

    Add Fast route packages for routes

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -1,11 +1,29 @@
 <?php 
 
 namespace Asad\Framework\Http;
+
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-       $content = '<h2>Hello World from Kernel</h2>';
-       return new Response(content:$content);
+     
+
+
+       $dispatcher = simpleDispatcher(function(RouteCollector $routeCollector){
+            $routeCollector->addRoute('GET','/',function(){
+                $content = '<h2>Hello World from Kernel</h2>';
+                return new Response(content:$content);
+            });
+       });
+
+       dump($dispatcher);
+        //create the dispatcher
+        
+        // opbtain the uri info
+        // call the handler, provided by the route info, and in order to create a infor
     }
 }

cb602d23e0b4eff632df3d222c051ab9d5d0489b
AsadullahQureshi <abduljabbarasadullah@gmail.com>
2024-07-29 10:23:34 +0500

    Create Container continue.. and phpunit error namespace resolve

---
+++ framework/tests/ContainerTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Asad\Framework\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase{
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
